07586188bb7f4463e228625ff32290c19be28b18
PavelSher <4829872@rambler.ru>
2020-01-24 17:00:25 +0200

    fixed_name_tests_by_author

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tests, through: :completed_tests
   has_many :completed_tests
-  has_many :tests, class_name: 'Test', foreign_key: :author_id
+  has_many :tests_by_author, class_name: 'Test', foreign_key: :author_id
   def completed_by_level(level)
     Test.joins('JOIN completed_tests ON tests.id = completed_tests.test_id')
     .where('completed_tests.user_id' => id, 'tests.level' => level)
