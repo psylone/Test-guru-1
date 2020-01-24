68f66ec9e81e82abc0e154137f919b07e1e423e3
PavelSher <4829872@rambler.ru>
2020-01-24 14:16:51 +0200

    user_tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,16 +1,15 @@
 class Test < ApplicationRecord
   belongs_to :category
-
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :questions
   has_many :completed_tests
   has_many :users, through: :completed_tests
+
   def self.names_by_category(category)
     Test.joins('JOIN categories ON tests.category_id = categories.id')
     .where('categories.title' => category)
     .order('tests.title DESC')
     .pluck('tests.title')
-
-
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_20_152612) do
+ActiveRecord::Schema.define(version: 2020_01_24_064325) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2020_01_20_152612) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "level", default: 0
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -64,4 +66,5 @@ ActiveRecord::Schema.define(version: 2020_01_20_152612) do
   add_foreign_key "completed_tests", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
